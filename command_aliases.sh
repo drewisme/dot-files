367c0b0c124d1b2e906e8df10087a8e35bb57229
Drew Partridge <drewisme@gmail.com>
2013-10-09 12:03:05 -0700

    Update command_aliases.sh

--- command_aliases.sh
+++ command_aliases.sh
@@ -5,3 +5,4 @@ alias crontab="VIM_CRONTAB=true crontab"
 alias flushcache="sudo killall -HUP mDNSResponder && dscacheutil -flushcache && sudo dscacheutil -flushcache"
 alias phperrors="find . -type f -name '*.php' -or -name '*.ctp' -exec php -l {} \; | grep -v '^No'"
 alias fucking='sudo'
+alias subl='"/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl"'
