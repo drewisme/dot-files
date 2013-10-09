1c2cb8589bdd91897afaed8042306dea8948ce63
Drew Partridge <drewisme@gmail.com>
2013-10-09 11:58:23 -0700

    Update ssh_aliases.sh

--- ssh_aliases.sh
+++ ssh_aliases.sh
@@ -1,17 +1,8 @@
-# Generic
-alias ccidev="ssh devsite.cc"
-alias htpc="ssh scott.usr.sh"
-alias home="htpc"
-
 # Work - Internal Clients
 work_servers=(iceman maverick goose)
 for server in "${work_servers[@]}"; do
 	alias $server="ssh ${server}.sidestudios.com"
 done
-
-alias ssprod="ssh 192.168.100.206"
-alias sscloud="ssh 108.171.190.242"
-alias sidedev="ssh 192.168.0.50"
 
 # Work - Billabong
 billabong_servers=(sundown blade viper jester merlin stinger chipper)
