279b0dc8bf1184e5fd854b2c6537f3cdbb8ac36c
BigToach <scott@sidestudios.com>
2013-11-25 01:00:44 -0800

    Checking OSTYPE for OS X specific commands. Fixing command prompt with history. Adding solr1 to the list of domains

--- ssh_aliases.sh
+++ ssh_aliases.sh
@@ -12,7 +12,7 @@ done
 
 # Work - Billabong Clusters
 billabong_clusters=(bbg rvca)
-billabong_servers=(lb1 fs1 db1 www1 www2 www3 www4 www5 cache1)
+billabong_servers=(lb1 fs1 db1 www1 www2 www3 www4 www5 cache1 solr1)
 for cluster in "${billabong_clusters[@]}"; do
 	for server in "${billabong_servers[@]}"; do
 		alias $server.$cluster="ssh ${server}.${cluster}.sidestudios.com"
